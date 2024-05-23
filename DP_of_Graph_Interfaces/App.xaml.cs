ca1ca4a34d98751ce4cc88ba86f2b03c844cf673
mickle <yasikk04.com>
2024-05-23 20:56:26 +0300

    Project created

---
+++ DP_of_Graph_Interfaces/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DP_of_Graph_Interfaces;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
